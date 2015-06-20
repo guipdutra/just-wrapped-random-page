77327dd0cac2457304ea7fc204306bde8225a88a
Guilherme Dutra <guipereira07@gmail.com>
2015-06-20 00:06:44 -0300

    Cleaning up

--- src/just_wrapped_random_page/core.clj
+++ src/just_wrapped_random_page/core.clj
@@ -2,12 +2,20 @@
   (require [clj-http.client :as client]
            [clojure.xml :as xml]))
 
-(def site-links
-  (map #(:content %) (map #(first %) (map #(:content %) (:content (xml/parse (java.io.ByteArrayInputStream. (.getBytes (:body (client/get "http://justwrapped.me/sitemap.xml"))))))))))
+(def site-url
+  "http://justwrapped.me/sitemap.xml")
+
+(defn parse-response-body [url]
+  (xml/parse (java.io.ByteArrayInputStream. (.getBytes (:body (client/get url))))))
+
+(defn extract-links [body]
+  (map #(:content %)
+       (map #(first %)
+            (map #(:content %) (:content body)))))
 
 
 (defn get-random-link [links]
-  (rand-nth links))
+  (first (rand-nth links)))
 
 (defn -main []
-  (println (get-random-link site-links)))
+  (println (get-random-link (extract-links (parse-response-body site-url)))))
